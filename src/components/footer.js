79de2aac456fa8e69ff2723d7708a2139327b5b1
Pratham <bhagatpratham101@gmail.com>
2021-06-16 11:32:22 +0530

    Add files via upload

---
+++ src/components/footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p className='footer'>
+        Project from{" "}
+        <a
+          className='links'
+          href='https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-random-quote-machine'
+        >
+          freecodecamp
+        </a>
+        Created by{" "}
+        <a className='links' href='https://github.com/bhagatpratham'>
+          Pratham
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
